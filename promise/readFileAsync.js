595fc1ae7b9bae76cd884c0299d8b04544536c1f
Polar <davidchen233@gmail.com>
2021-10-16 10:55:42 +0800

    新增asyn 版本readFile

---
+++ promise/readFileAsync.js
@@ -0,0 +1,24 @@
+let fs = require("fs");
+
+function readFilePromise() {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./input.txt", "utf-8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+}
+
+async function readFile() {
+  try {
+    const file = await readFilePromise();
+    console.log("data", file);
+  } catch (err) {
+    console.error("err", err);
+  }
+}
+
+readFile();
