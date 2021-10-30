7f494242e794fefc917d18fd2bbe50f0d88dafe6
Polar <davidchen233@gmail.com>
2021-10-30 14:41:51 +0800

    add port: process.env.DB_PORT

--- todos-be/server.js
+++ todos-be/server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
 });
